4797e20374f6cc63512b224a9addfe62b1c80ca4
Arjun R <arjun121094@gmail.com>
2025-04-27 14:48:36 +0000

    monthly avg coding complete

--- dbt/macros/date_utils.sql
+++ dbt/macros/date_utils.sql
@@ -1,6 +1,14 @@
 {% macro get_month_name(date_column) %}
-    {{ 
-        -- Cross-database compatible
-        adapter.dispatch('get_month_name')(date_column)
-    }}
+    {%- if target.type == 'snowflake' -%}
+        TO_CHAR({{ date_column }}, 'MMMM')
+    {%- elif target.type == 'bigquery' -%}
+        FORMAT_DATE('%B', {{ date_column }})
+    {%- elif target.type == 'redshift' or target.type == 'postgres' -%}
+        TO_CHAR({{ date_column }}, 'Month')
+    {%- elif target.type == 'databricks' or target.type == 'spark' -%}
+        date_format({{ date_column }}, 'MMMM')
+    {%- else -%}
+        -- Default fallback
+        {{ exceptions.raise_compiler_error("month_name macro not implemented for " ~ target.type) }}
+    {%- endif -%}
 {% endmacro %}
