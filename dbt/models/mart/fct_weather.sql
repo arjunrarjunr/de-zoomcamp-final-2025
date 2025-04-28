35bb18049a953356d5ce9652efd8e3d8f03b5757
arjunrarjunr <arjun121094@gmail.com>
2025-04-28 11:50:48 +0530

    Change incremental strategy from 'insert_overwrite' to 'merge' in fct_weather.sql

--- dbt/models/mart/fct_weather.sql
+++ dbt/models/mart/fct_weather.sql
@@ -2,7 +2,7 @@
     config(
         materialized='incremental',
         unique_key='rec_id',
-        incremental_strategy='insert_overwrite',
+        incremental_strategy='merge',
         on_schema_change='append_new_columns'
     )
 }}
